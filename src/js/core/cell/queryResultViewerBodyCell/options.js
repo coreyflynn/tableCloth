868aa9ddac6601c7715cda1c3a5ee36ada381e37
Corey Flynn <flynn.corey@gmail.com>
2015-06-05 17:01:37 -0400

    cellId default in bodyCell

--- src/js/core/cell/queryResultViewerBodyCell/options.js
+++ src/js/core/cell/queryResultViewerBodyCell/options.js
@@ -4,7 +4,7 @@
 
 function configure(options) {
   options = (options === undefined) ? {} : options;
-  options.cellID = (options.cellID === undefined) ? 'CellLine' : options.cellID;
+  options.cellId = (options.cellId === undefined) ? 'CellLine' : options.cellId;
   options.score = (options.score === undefined) ? Math.random() * 200 - 100 : options.score;
   options.cellColor = (options.cellColor === undefined) ? '#f47222' : options.cellColor;
 
